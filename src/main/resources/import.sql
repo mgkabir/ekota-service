8ce0fcd06b6983351e5b725a52a711541169c9c9
Mohammad Kabir <mgkabir@gmail.com>
2018-12-11 11:29:21 +1100

    sample tenant and account data added

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -10,3 +10,14 @@ INSERT INTO app_user (id, first_name, last_name, password, username) VALUES (2, 
 INSERT INTO user_role(user_id, role_id) VALUES(1,1);
 INSERT INTO user_role(user_id, role_id) VALUES(2,1);
 INSERT INTO user_role(user_id, role_id) VALUES(2,2);
+
+INSERT INTO tenants(id, name) VALUES(1,'Tenant - 01');
+INSERT INTO tenants(id, name) VALUES(2,'Tenant - 02');
+
+INSERT INTO accounts(id, mobile_number, name, tenant_id) VALUES(1,'+8801976452247', 'Acc Name 01 - Tenant 01', 1);
+INSERT INTO accounts(id, mobile_number, name, tenant_id) VALUES(2,'+8801776452248', 'Acc Name 02 - Tenant 01', 1);
+INSERT INTO accounts(id, mobile_number, name, tenant_id) VALUES(3,'+8801876452249', 'Acc Name 03 - Tenant 01', 1);
+
+INSERT INTO accounts(id, mobile_number, name, tenant_id) VALUES(4,'+8801976552247', 'Acc Name 04 - Tenant 02', 2);
+INSERT INTO accounts(id, mobile_number, name, tenant_id) VALUES(5,'+8801776652248', 'Acc Name 05 - Tenant 02', 2);
+INSERT INTO accounts(id, mobile_number, name, tenant_id) VALUES(6,'+8801876752249', 'Acc Name 06 - Tenant 02', 2);
